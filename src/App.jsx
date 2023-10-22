ae9fdf48ad49631fc2107bdfd37c09327d6f983a
Steph <108551327+s-hens@users.noreply.github.com>
2023-10-22 17:56:11 +0100

    Introduce first useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-//import { useState } from 'react'
+///////////////////////
+// CONTROLLING STATE //
+///////////////////////
+
+// Step 1. Import useState hook. It comes with React by default.
+import { useState } from "react"
 
 function App() {
-  //const [count, setCount] = useState(0)
+  const colors = ["pink", "green", "blue", "yellow", "purple"];
+
+  // Step 2. Define state using this pattern:
+  // const [stateValue, setStateValue] = useState(initialValue);
+  const [backgroundColor, setBackgroundColor] = useState(colors[0]);
 
   return (
+    // Step 3. stateValue can be used as a variable. Until changed, it will have the initial value set.
     <>
+    <main style={{backgroundColor}}>
       Starting from a blank slate!
+    </main>
     </>
   )
 }
