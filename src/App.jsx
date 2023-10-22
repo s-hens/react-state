42986341e9ea01336c642421fff6cc7466113726
Steph <108551327+s-hens@users.noreply.github.com>
2023-10-22 19:51:28 +0100

    Create new state changeCount to track how many times state backgroundColor is changed

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,29 @@ import { useState } from "react"
 function App() {
   const colors = ["pink", "green", "blue", "yellow", "purple"];
 
+  const handleClick = (color) => () => {
+    setBackgroundColor(color);
+    if (color != backgroundColor) {
+      setChangeCount((changeCount) => changeCount + 1);
+    }
+  };
+
   const buttons = colors.map(color => 
-    <button type="button" key={color} onClick={() => setBackgroundColor(color)}>{color}</button>
+    <button type="button" key={color} onClick={handleClick(color)}>{color}</button>
   )
 
   // Step 2. Define state using this pattern:
   // const [stateValue, setStateValue] = useState(initialValue);
   const [backgroundColor, setBackgroundColor] = useState(colors[0]);
 
+  const [changeCount, setChangeCount] = useState(0);
+
   return (
     // Step 3. stateValue can be used as a variable. Until changed by calling setStateValue(x), it will have the initial value set.
     <>
     <main style={{backgroundColor}}>
       <div>{buttons}</div>
+      State backgroundColor has been changed {changeCount} time(s).
     </main>
     </>
   )
